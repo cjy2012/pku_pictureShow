ba913c2e5e82e3ad8befefe45b2e6bd9dc1d4638
zhangwei <whucszhangwei@gmail.com>
2016-06-10 20:21:24 +0800

    微信验证修改

---
+++ config/wechatcfg.js
@@ -0,0 +1,5 @@
+module.exports={
+    token:'pictureboard',
+    encodingAESKey:'mAMDJhofznDT5PWDKFnrDD1C9MVzM67uGTChp8JuSIG',
+    corpId:'wx1d3765eb45497a18'
+}
--- util/verify.js
+++ util/verify.js
@@ -1,17 +1,25 @@
 var express = require('express');  
-var app = express();  
+var app = express();
+var wechatCrypto=require('wechat-crypto');
+var config = require('../config/wechatcfg');
   
 function toWeb(req,res) {  
     res.status(200).send("User Message");  
 }  
   
 function verifyServer(req,res) {  
-     var echostr = req.query.echostr;  
+     /*var echostr = req.query.echostr;  
      var sign = req.query.msg_signature;  
      var timestamp = req.query.timestamp;  
      var nonce = req.query.nonce;  
      console.log('recv weixin req:'," sign",sign,"timestamp",timestamp,"nonce",nonce,"echostr",echostr);  
-     res.status(200).send(""+echostr);  
+
+     res.status(200).send(""+echostr); */
+    var echostr = req.query.echostr;
+    var crypto = new WXBizMsgCrypt(config.token, config.encodingAESKey, config.corpId);
+    var s = crypto.decrypt(echostr);
+    console.log('echostr 解密后：',s);
+    res.send(s.message);
 }  
   
 app.get('/test',function(req,res) {  
